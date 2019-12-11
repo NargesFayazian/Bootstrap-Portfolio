e4f81dc3d91635413e3141ea5146817143d00fff
nargesfamili <narges.fayazian@gmail.com>
2019-12-10 19:53:48 -0500

    v6

---
+++ resume.js
@@ -0,0 +1,29 @@
+// ======================================menu=================================================
+var i = 0;
+          function expand() {
+            if (i == 0) {
+              document.getElementById("toggle").style.top = "100px";
+              document.getElementById("toggle").style.right = "120px";
+              document.getElementById("menu").style.top = "100px";
+              document.getElementById("menu").style.right = "120px";
+              document.getElementById("menu").style.transform = "scale(3)";
+              document.getElementById("plus").style.transform = "rotate(45deg)";
+  
+              i = 1;
+            }
+            else {
+              document.getElementById("toggle").style.top = "20px";
+              document.getElementById("toggle").style.right = "20px";
+              document.getElementById("menu").style.top = "20px";
+              document.getElementById("menu").style.right = "20px";
+              document.getElementById("menu").style.transform = "scale(0)";
+              document.getElementById("plus").style.transform = "rotate(0deg)";
+  
+              i = 0;
+            }
+          }
+        // ===================================================================================
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+            $(".diagonal-bg svg line").attr("stroke-width", ((30 + scroll / 10) + "%"));
+        });
